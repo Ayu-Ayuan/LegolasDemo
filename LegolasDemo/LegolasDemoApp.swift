ed9d3dae603167fb36ab39058ceda776a4fc7913
Ayuan <zhanyuan0108@qq.com>
2025-01-16 10:43:33 +0800

    Initial Commit

---
+++ LegolasDemo/LegolasDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegolasDemoApp.swift
+//  LegolasDemo
+//
+//  Created by Ayuano on 2025/1/16.
+//
+
+import SwiftUI
+
+@main
+struct LegolasDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
